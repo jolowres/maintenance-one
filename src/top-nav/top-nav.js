07b1df6ceedaf78c8c1610e1d3123423d9cb9bb0
jolowres <jolowres@gmail.com>
2018-09-09 13:11:44 +0100

    adding top nav

---
+++ src/top-nav/top-nav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './top-nav.css'
+
+class TopNav extends Component {
+  render() {
+    return (
+      <div className="top-nav">
+        <p className="top-nav__text">MAINTENANCE ONE</p>
+      </div>
+    );
+  }
+}
+
+export default TopNav
