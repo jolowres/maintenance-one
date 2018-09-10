855016ee15ca7e075113cdcb8c5243bce476ea68
jolowres <jolowres@gmail.com>
2018-09-10 15:58:31 +0100

    changed to bootsrtap nav

--- src/components/top-nav/top-nav.js
+++ src/components/top-nav/top-nav.js
@@ -5,11 +5,23 @@ import './top-nav.css'
 class TopNav extends Component {
   render() {
     return (
-      <div className="top-nav">
-        <p>MAINTENANCEone</p>
-        <Link to='/'>Home</Link>
-        <Link to='/product'>Product</Link>
-      </div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">Maintenance one</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+             <Link className="nav-link"  to='/'>Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link"  to='/product'>Product</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
