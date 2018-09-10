637c2b642a907b4e345cfa7a797e17314541435f
jolowres <jolowres@gmail.com>
2018-09-10 13:02:15 +0100

    simple route

---
+++ src/home/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './home.css'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome to the app</p>
+      </div>
+    );
+  }
+}
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -4,9 +4,21 @@ import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import TopNav from './top-nav/top-nav'
+import Home from './home/home'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <div>
+    <TopNav></TopNav>
+    <BrowserRouter>
+      <Route path="/" component={ Home }/>
+    </BrowserRouter>
+  </div>, document.getElementById('root')
+);
 registerServiceWorker();
