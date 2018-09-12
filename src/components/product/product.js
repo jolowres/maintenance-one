a764fc38e3ea0a661be8f7ed2c52cd004d43341a
jolowres <jolowres@gmail.com>
2018-09-12 12:45:33 +0100

    wip - form bound

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Product = (props) => {
-  return (
-    <div>
-      <h2>Product details</h2>
-      <div>{props.product.name}</div>
-      <div>{props.product.productCode}</div>
-    </div>
-  )
+class Product extends Component {
+
+  render () {
+    console.log('----->')
+    console.log(this.props)
+    console.log('<-----')
+    if (this.props.product) {
+      return (
+        <div>             
+          <h2>Product details</h2>
+          <form>
+            <div className="row">
+              <div className="col">
+                  <label htmlFor="product-name">Product name</label>
+                  <input className="form-control" id="product-name" value={this.props.product.name}/>
+              </div>
+              <div className="col">
+                <label htmlFor="product-code">Product code</label>
+                {/* <input className="form-c      ontrol" id="product-code" value={props.product.productCode}/> */}
+              </div>
+            </div>
+          </form>
+        </div>
+      )
+    } else {
+      return (
+        <span>Loading...</span>
+      )
+    }
+  }
 }
 
 export default Product
--- src/containers/product-container.js
+++ src/containers/product-container.js
@@ -5,14 +5,27 @@ import Product from '../components/product/product'
 
 class ProductContainer extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      product: this.props.product.product
+    }
+  }
+
   componentDidMount() {     
     const { dispatch, product } = this.props;
     dispatch(fetchProduct(product));
   }
 
+  componentWillReceiveProps(nextProps) {
+    console.log('new props --> updating state')
+    console.log(nextProps.product)
+    this.setState({product: nextProps.product})
+  }
+
   render() {
     return (
-      <Product product={this.props.product}></Product>
+      <Product product={this.state.product}></Product>
     )
   }
 
