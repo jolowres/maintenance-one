d4fe816add1099be32bde24968f09ea9b45eda3b
jolowres <jolowres@gmail.com>
2018-09-20 16:51:31 +0100

    product list displaying with style

--- src/components/product-list/product-list.js
+++ src/components/product-list/product-list.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'
+import './product-list.css'
 
 class ProductList extends Component {
   render () {
     if (this.props.products) {
       const productListItems = this.props.products.map((product) => 
-        <div key={product.productCode}>
-          <span>{product.productCode}</span>
-          <span>{product.name}</span>
+        <div className="row data-row" key={product.productCode}>
+          <div className="col-2">{product.productCode}</div>
+          <div className="col-9">{product.name}</div>
+          <div className="col-1">
+            <button className="btn btn-sm  btn-primary">Edit</button>
+          </div>
         </div>
       )
       return (
         <div>
+          <div className="row header">
+            <div className="col-2">Code</div>
+            <div className="col-9">Name   </div>
+            <div className="col-1">Action   </div>
+          </div>
           <div>{productListItems}</div>
         </div>
       )
