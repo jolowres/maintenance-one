9bab30efb9af0837a9dd06a4d7604d37257a8b0c
jolowres <jolowres@gmail.com>
2018-09-21 16:10:24 +0100

    not working but progress

---
+++ src/components/product/product.test.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import { render } from 'react-testing-library';
+// import Product from './product';
+// import 'react-testing-library/cleanup-after-each'
+// import 'jest-dom/extend-expect';
+
+// it('renders without crashing', () => {
+  // const product = {
+  //   productCode: 'HCP'
+  // };
+  // const { componentUnderTest } = render(<Product props={product}/>);
+  // expect(componentUnderTest('HCP')).toBeInTheDOM();
+// });
