c54b038d97b137c9548ec47345106fe3d8bd85e1
jolowres <jolowres@gmail.com>
2018-10-04 12:59:52 +0100

    products.actions.test

--- src/actions/product.actions.test.js
+++ src/actions/product.actions.test.js
@@ -1,30 +1,30 @@
-import * as actions from './product.actions'
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
-import axios from 'axios'
-import AxiosMockAdapter from 'axios-mock-adapter'
+  import * as actions from './product.actions'
+  import configureMockStore from 'redux-mock-store'
+  import thunk from 'redux-thunk'
+  import axios from 'axios'
+  import AxiosMockAdapter from 'axios-mock-adapter'
 
-const middlewares = [thunk]
-const mockStore = configureMockStore(middlewares)
-const axiosMock = new AxiosMockAdapter(axios)
+  const middlewares = [thunk]
+  const mockStore = configureMockStore(middlewares)
+  const axiosMock = new AxiosMockAdapter(axios)
 
 
-describe('product.actions', () => {
+  describe('product.actions', () => {
 
-  describe('fetchProduct', () => {
-    it('should retrieve product', () => {
-      expect.assertions(1)
-      axiosMock.onAny().reply(200, {
-        name: 'test',
-        productCode: 'testCode'
-      })
-      const store = mockStore({product: ''})
-      return store.dispatch(actions.fetchProduct()).then(() => {
-        let myResult = store.getActions().find(action => action.type == 'RECEIVE_PRODUCT')
-        expect(myResult.product.name).toEqual('test')
+    describe('fetchProduct', () => {
+      it('should retrieve product', () => {
+        expect.assertions(1)
+        axiosMock.onAny().reply(200, {
+          name: 'test',
+          productCode: 'testCode'
+        })
+        const store = mockStore({product: ''})
+        return store.dispatch(actions.fetchProduct()).then(() => {
+          let myResult = store.getActions().find(action => action.type == 'RECEIVE_PRODUCT')
+          expect(myResult.product.name).toEqual('test')
+        })
       })
     })
-  })
 
   describe('updateProduct', () => {
     it('should update product', () => {
